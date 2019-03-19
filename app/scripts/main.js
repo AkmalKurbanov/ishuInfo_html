9524c7e19773037d97f9d91a7285bf4458f5b699
AkmalKurbanov <250296a@gmail.com>
2019-03-19 19:54:54 +0600

    ---

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,16 +18,68 @@ $(document).ready(function () {
   });
   $('#registration-tab').on('click', function () {
     $('.registration__modalWindow').css('flex-direction', 'row-reverse');
+    $('.closeBtn').addClass('closeBtnLeft');
   });
   $('#signIn-tab').on('click', function () {
     $('.registration__modalWindow').css('flex-direction', 'row');
+    $('.closeBtn').removeClass('closeBtnLeft');
   });
+  $('.registration-js').on('click', function () {
+    $('.blurEffect').css('display', 'flex');
+  });
+  $('.closeBtn').on('click', function () {
+    $('.blurEffect').hide();
+  });
+
+
+
   // modal windows end
 
 
+  // hamburger
+  $('.hamburger').click(function () {
+    $(this).toggleClass('openClose');
+    $('.mnu-js').toggleClass('mobileMnu-js');
+    $('.hamburger__line').toggleClass('lineBg');
+  });
 
+  $('.mnu-js').click(function () {
+    $('.hamburger').removeClass('openClose');
+    $('.hamburger__line').removeClass('lineBg');
+  });
 
+  $(document).mouseup(function (e) {
+    var div = $(".hamburger");
+    if (!div.is(e.target) &&
+      div.has(e.target).length === 0) {
+      div.removeClass('openClose');
+    }
+  });
 
+  // hamburger end
+
+  // mmenu
+  $('.header__mnuFirstLvl').clone().appendTo('.mmenu-nav');
+
+  var $menu = $('.mmenu-nav').mmenu({
+    navbar: {
+      title: 'IshuInfo'
+    },
+    extensions: ['fx-menu-slide', 'fx-listitems-slide', 'border-full', 'pagedim-black', 'position-right'],
+    offCanvas: {
+
+    },
+    counters: true
+  });
+
+  var $icon = $('.hamburger');
+  var API = $menu.data('mmenu');
+
+  $icon.on('click', function () {
+    API.open();
+  });
+
+  // mmenu
 
 
 
